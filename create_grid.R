d26d4d519d2dad023d328e52312a753cc993453a
charleygros <charley.gros@gmail.com>
2020-12-30 17:11:22 +1100

    create_grid: crop

--- create_grid.R
+++ create_grid.R
@@ -3,7 +3,7 @@ library(raster)
 # env_folder = "R:/IMAS/Antarctic_Seafloor/environmental_data/500mResolution"
 # env_list = list("depth"="_bathy500m_shelf_gebco2020_depth.gri", "slope"="_bathy500m_shelf_gebco2020_slope.gri", "npp"="_NPP_SummerAverage_shelf.gri", "currents"="_waom2k_seafloorcurrents_500mInterpolation_shelf.gri", "temp"="_waom2k_seafloortemperature_500mInterpolation_shelf.gri")
 # extent = extent(-1.05e6, -1e6, 1e6, 1.05e6)
-# fname_out = "C:/Users/cgros/data/20201207_coralnetfull/count/SXY.csv"
+# fname_out = "C:/Users/cgros/data/20201207_coralnetfull/count/grid.csv"
 
 create_env_grid <- function(env_folder, env_list, extent, fname_out=NULL) {
   out_df = NULL
@@ -12,12 +12,13 @@ create_env_grid <- function(env_folder, env_list, extent, fname_out=NULL) {
     env_file <- paste(c(env_folder, 
                         paste0("Circumpolar_EnvData", env_list[env_name])), 
                       collapse="/")
-    layer <- raster(env_file, ext=extent)
+    layer <- raster(env_file)
+    layer_crop <- crop(layer, extent)
     # Convert to dataframe
-    layer_df <- as.data.frame(layer, xy=TRUE, na.rm=TRUE)
+    layer_df <- as.data.frame(layer_crop, xy=TRUE, na.rm=TRUE)
     # Add col name
     colnames(layer_df) <- c("Longitude", "Latitude", env_name)
-    
+
     if (is.null(out_df)) {
       out_df = layer_df
     }
